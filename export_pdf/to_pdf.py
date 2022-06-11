d24feef8ecede433b5813c5755d0f4dd32859aac
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2022-06-11 16:27:51 +0700

    change from weasyprint to reportlab

--- export_pdf/to_pdf.py
+++ export_pdf/to_pdf.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import json
-from jinja2 import Environment, FileSystemLoader
-from weasyprint import HTML
+from reportlab.platypus import Paragraph, Spacer, Table, Image, SimpleDocTemplate, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import letter
+from sklearn import datasets
 
 def export_to_pdf(json_filename, output_filename):
     file = open(filename, 'r')
@@ -11,13 +14,21 @@ def export_to_pdf(json_filename, output_filename):
     univ = df.loc[0, 'university']
     skills = df.loc[0, 'skills']
     profession = df.loc[0, 'profession']
-    env=Environment(loader=FileSystemLoader('export_pdf/templates'))
-    template=env.get_template('report_template.html')
-    template_vars = {
-        "name" : name,
-        "university" : univ,
-        "skills" : skills,
-        "profession": profession
-    }
-    html_out = template.render(template_vars)
-    HTML(string=html_out).write_pdf(filename+'_summary.pdf')
+
+file = open('dataset.json', 'r')
+data=json.load(file)
+df=pd.json_normalize(data)
+name = df.loc[0, 'name']
+univ = df.loc[0, 'university']
+skills = df.loc[0, 'skills']
+profession = df.loc[0, 'profession']
+title_name="Test"
+output_filename='test2.pdf'
+user_info=f"Name : {name} Institution: {univ} Skills: {skills} Profession: {profession}"
+document=["name", "university", "skills", "profession"]
+report=SimpleDocTemplate(output_filename, pagesize=A4)
+styles = getSampleStyleSheet()
+empty_line=Spacer(0,20)
+ReportTitle = Paragraph(title_name)
+UserInformartion = Paragraph(user_info)
+report.build([ReportTitle,empty_line,UserInformartion])
